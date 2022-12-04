ab4ce4d042e95097f6b3fa1c6a8a268628c7d37a
Roland Ebrecht <roland.ebrecht@hs-augsburg.com>
2022-12-04 15:01:16 +0100

    Working Version V1.0
    * Included neighbors calculation
    * Included game of life rules
    * Included time for update handling
    * Added random initialization
    * Added iterator over canvas

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,60 @@
 use itertools::Itertools;
 use nannou::prelude::*;
+use rand::random;
 
-const SCALE: f32 = 20.0;
-const GRID_SIZE_X: f32 = 40.0;
-const GRID_SIZE_Y: f32 = 40.0;
+const SCALE: f32 = 5.0;
+const GRID_SIZE_X: f32 = 150.0;
+const GRID_SIZE_Y: f32 = 150.0;
+const UPDATE_TIME: f32 = 0.05;
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
+#[derive(Copy, Clone)]
 struct Canvas {
-    canvas: [[u8; GRID_SIZE_X as usize]; GRID_SIZE_Y as usize],
+    elements: [[u8; GRID_SIZE_X as usize]; GRID_SIZE_Y as usize],
 }
 
 impl Canvas {
     fn new() -> Self {
-        Self {
-            canvas: [[0; GRID_SIZE_X as usize]; GRID_SIZE_Y as usize],
+        let mut temp: Canvas = Canvas {
+            elements: [[0; GRID_SIZE_X as usize]; GRID_SIZE_Y as usize],
+        };
+
+        // for ((x,y),element) in &mut temp.iter_elements() {
+        //     *element = random::<u8>() % 2;
+        // }
+
+        for x in 0..GRID_SIZE_X as usize {
+            for y in 0..GRID_SIZE_Y as usize {
+                temp.elements[x][y] = random::<u8>() % 2;
+            }
         }
+        temp
     }
 
-    fn get_value(&self, x: isize, y: isize) -> u8 {
+    pub fn iter_elements(&self) -> impl Iterator<Item = ((usize, usize), &u8)> {
+        self.elements.iter().enumerate().flat_map(|(x, row)| {
+            row.iter()
+                .enumerate()
+                .map(move |(y, column)| ((x, y), column))
+        })
+    }
+
+    fn get_value(&self, mut x: isize, mut y: isize) -> u8 {
         x = (x + GRID_SIZE_X as isize) % GRID_SIZE_X as isize;
         y = (y + GRID_SIZE_Y as isize) % GRID_SIZE_Y as isize;
-        self.canvas[x as usize][y as usize]
+        self.elements[x as usize][y as usize]
     }
 
-    fn getNeighbors(&self, x: usize, y: usize) -> usize {
+    fn get_neighbors(&self, x: usize, y: usize) -> usize {
         let mut number: u8 = 0;
 
         for (dx, dy) in [-1, 0, 1].iter().cartesian_product([-1, 0, 1].iter()) {
-            number += self.get_value(x as isize + dx, y as isize + dy);
+            if *dx != 0 || *dy != 0 {
+                number += self.get_value(x as isize + dx, y as isize + dy);
+            }
         }
         number as usize
     }
@@ -38,28 +62,51 @@ impl Canvas {
 
 struct Model {
     screen: Canvas,
+    t: f32,
 }
 
 impl Model {
     fn new() -> Self {
         Self {
             screen: Canvas::new(),
+            t: 0.0,
         }
     }
 
     fn draw(&self, draw: &Draw) {
-        for y in 0..GRID_SIZE_Y as usize {
-            for x in 0..GRID_SIZE_X as usize {
+        for ((x, y), element) in self.screen.iter_elements() {
+            if *element == 1 {
                 draw.rect()
                     .x_y(x as f32, y as f32)
                     .w_h(0.9, 0.9)
-                    .color(self.screen.canvas[x][y]);
+                    .color(BLACK);
             }
         }
     }
 }
 
-fn update(_app: &App, _m: &mut Model, _update: Update) {}
+fn update(app: &App, m: &mut Model, _update: Update) {
+    // Handle time
+    if app.time - m.t > UPDATE_TIME {
+        m.t = app.time;
+        // Get current stage
+        let leap = m.screen;
+
+        for ((x, y), element) in leap.iter_elements() {
+            m.screen.elements[x][y] = match leap.get_neighbors(x, y) {
+                0 | 1 | 4 | 5 | 6 | 7 | 8 => 0, // Death
+                n @ 2..=3 => {
+                    if *element == 0 && n == 2 {
+                        0 // stay dead
+                    } else {
+                        1 // live
+                    }
+                }
+                _ => 0,
+            }
+        }
+    }
+}
 
 fn event(_app: &App, _m: &mut Model, event: WindowEvent) {
     match event {
